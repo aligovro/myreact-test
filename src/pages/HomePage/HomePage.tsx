d60495a3e021dd5de24985de6617ac245a7c851e
aligovro <aligovro@gmail.com>
2024-11-11 20:40:46 +0300

    first-commit

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './HomePage.module.scss';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className={styles.homepage}>
+      <h1 className={styles.homepage__title}>Главная страница</h1>
+      <p className={styles.homepage__description}>
+        Добро пожаловать на сайт! Выберите товар для просмотра.
+      </p>
+      <Link to="/product/1" className={styles.homepage__link}>
+        Перейти к товару 1
+      </Link>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/ProductPage/ProductPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './ProductPage.module.scss';
+
+const ProductPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+
+  return (
+    <div className={styles['product-page']}>
+      <h1 className={styles['product-page__title']}>Страница товара</h1>
+      <p className={styles['product-page__info']}>Информация о товаре с ID: {id}</p>
+      <button className={styles['product-page__button']}>Купить</button>
+    </div>
+  );
+};
+
+export default ProductPage;
